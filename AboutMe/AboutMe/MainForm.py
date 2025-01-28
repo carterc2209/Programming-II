ece232bd070172922f505bc153635cdaabb74eab
carterc2209 <campbell.c@stu.janesville.k12.wi.us>
2025-01-28 14:54:46 -0600

    About me 1/28

---
+++ AboutMe/AboutMe/MainForm.py
@@ -0,0 +1,66 @@
+﻿import System.Drawing
+import System.Windows.Forms
+
+from System.Drawing import *
+from System.Windows.Forms import *
+
+class MainForm(Form):
+    def __init__(self):
+        self.InitializeComponent()
+    
+    def InitializeComponent(self):
+        self._label1 = System.Windows.Forms.Label()
+        self._button1 = System.Windows.Forms.Button()
+        self._button2 = System.Windows.Forms.Button()
+        self.SuspendLayout()
+        # 
+        # label1
+        # 
+        self._label1.BackColor = System.Drawing.Color.White
+        self._label1.Font = System.Drawing.Font("Microsoft Sans Serif", 24, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+        self._label1.Location = System.Drawing.Point(36, 27)
+        self._label1.Name = "label1"
+        self._label1.Size = System.Drawing.Size(633, 366)
+        self._label1.TabIndex = 0
+        # 
+        # button1
+        # 
+        self._button1.BackColor = System.Drawing.Color.FromArgb(255, 128, 128)
+        self._button1.Font = System.Drawing.Font("Microsoft Sans Serif", 15.75, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+        self._button1.Location = System.Drawing.Point(56, 453)
+        self._button1.Name = "button1"
+        self._button1.Size = System.Drawing.Size(216, 102)
+        self._button1.TabIndex = 1
+        self._button1.Text = "Show"
+        self._button1.UseVisualStyleBackColor = False
+        self._button1.Click += self.Button1Click
+        # 
+        # button2
+        # 
+        self._button2.BackColor = System.Drawing.Color.FromArgb(255, 128, 128)
+        self._button2.Font = System.Drawing.Font("Microsoft Sans Serif", 15.75, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+        self._button2.Location = System.Drawing.Point(431, 453)
+        self._button2.Name = "button2"
+        self._button2.Size = System.Drawing.Size(216, 102)
+        self._button2.TabIndex = 2
+        self._button2.Text = "Exit"
+        self._button2.UseVisualStyleBackColor = False
+        self._button2.Click += self.Button2Click
+        # 
+        # MainForm
+        # 
+        self.BackColor = System.Drawing.Color.FromArgb(128, 128, 255)
+        self.ClientSize = System.Drawing.Size(710, 590)
+        self.Controls.Add(self._button2)
+        self.Controls.Add(self._button1)
+        self.Controls.Add(self._label1)
+        self.Name = "MainForm"
+        self.Text = "AboutMe"
+        self.ResumeLayout(False)
+
+
+    def Button2Click(self, sender, e):
+        Application.Exit()
+
+    def Button1Click(self, sender, e):
+        self._label1.Text = "I am taking Geometry Honors and Algrebra II Honors next year."
