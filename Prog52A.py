db5badd17b0278a4ddbdbaaa7cfc78ab935948f7
carterc2209 <campbell.c@stu.janesville.k12.wi.us>
2025-01-28 20:56:42 +0000

    hello & prog52a 1/28

---
+++ Prog52A.py
@@ -0,0 +1,6 @@
+length = int(input("Enter length: "))
+width = int(input("Enter width: "))
+area = length * width
+perimeter = 2*length + 2*width
+print("Area: ", area)
+print("Perimeter: ", perimeter)
