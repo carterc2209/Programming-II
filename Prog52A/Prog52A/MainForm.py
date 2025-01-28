b9d6ff5a0bab4648ec77004d4a5ecbefcea2f1d7
carterc2209 <campbell.c@stu.janesville.k12.wi.us>
2025-01-28 15:27:12 -0600

    idk

---
+++ Prog52A/Prog52A/MainForm.py
@@ -0,0 +1,90 @@
+﻿import System.Drawing
+import System.Windows.Forms
+
+from System.Drawing import *
+from System.Windows.Forms import *
+
+class MainForm(Form):
+    def __init__(self):
+        self.InitializeComponent()
+    
+    def InitializeComponent(self):
+        self._label1 = System.Windows.Forms.Label()
+        self._textBox1 = System.Windows.Forms.TextBox()
+        self._textBox2 = System.Windows.Forms.TextBox()
+        self._label2 = System.Windows.Forms.Label()
+        self._label3 = System.Windows.Forms.Label()
+        self._label4 = System.Windows.Forms.Label()
+        self.SuspendLayout()
+        # 
+        # label1
+        # 
+        self._label1.BackColor = System.Drawing.Color.White
+        self._label1.Font = System.Drawing.Font("Microsoft Sans Serif", 20.25, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+        self._label1.Location = System.Drawing.Point(42, 26)
+        self._label1.Name = "label1"
+        self._label1.Size = System.Drawing.Size(293, 40)
+        self._label1.TabIndex = 0
+        self._label1.Text = "Enter Area:"
+        # 
+        # textBox1
+        # 
+        self._textBox1.Font = System.Drawing.Font("Microsoft Sans Serif", 21.75, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+        self._textBox1.Location = System.Drawing.Point(194, 26)
+        self._textBox1.Name = "textBox1"
+        self._textBox1.Size = System.Drawing.Size(677, 40)
+        self._textBox1.TabIndex = 1
+        # 
+        # textBox2
+        # 
+        self._textBox2.Font = System.Drawing.Font("Microsoft Sans Serif", 21.75, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+        self._textBox2.Location = System.Drawing.Point(209, 93)
+        self._textBox2.Name = "textBox2"
+        self._textBox2.Size = System.Drawing.Size(662, 40)
+        self._textBox2.TabIndex = 3
+        # 
+        # label2
+        # 
+        self._label2.BackColor = System.Drawing.Color.White
+        self._label2.Font = System.Drawing.Font("Microsoft Sans Serif", 20.25, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+        self._label2.Location = System.Drawing.Point(42, 93)
+        self._label2.Name = "label2"
+        self._label2.Size = System.Drawing.Size(293, 40)
+        self._label2.TabIndex = 2
+        self._label2.Text = "Enter Width:"
+        # 
+        # label3
+        # 
+        self._label3.BackColor = System.Drawing.Color.White
+        self._label3.Font = System.Drawing.Font("Microsoft Sans Serif", 20.25, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+        self._label3.Location = System.Drawing.Point(42, 161)
+        self._label3.Name = "label3"
+        self._label3.Size = System.Drawing.Size(829, 40)
+        self._label3.TabIndex = 4
+        self._label3.Text = "Area:"
+        # 
+        # label4
+        # 
+        self._label4.BackColor = System.Drawing.Color.White
+        self._label4.Font = System.Drawing.Font("Microsoft Sans Serif", 20.25, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+        self._label4.Location = System.Drawing.Point(42, 219)
+        self._label4.Name = "label4"
+        self._label4.Size = System.Drawing.Size(829, 40)
+        self._label4.TabIndex = 5
+        self._label4.Text = "Perimeter:"
+        # 
+        # MainForm
+        # 
+        self.BackColor = System.Drawing.Color.FromArgb(128, 128, 255)
+        self.ClientSize = System.Drawing.Size(931, 587)
+        self.Controls.Add(self._label4)
+        self.Controls.Add(self._label3)
+        self.Controls.Add(self._textBox2)
+        self.Controls.Add(self._label2)
+        self.Controls.Add(self._textBox1)
+        self.Controls.Add(self._label1)
+        self.Name = "MainForm"
+        self.Text = "Prog52A"
+        self.ResumeLayout(False)
+        self.PerformLayout()
+
