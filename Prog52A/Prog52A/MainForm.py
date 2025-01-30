492d3062f14c1effc7d7c645777edc2047c7b10a
carterc2209 <campbell.c@stu.janesville.k12.wi.us>
2025-01-30 15:02:38 -0600

    Progs 1/30

--- Prog52A/Prog52A/MainForm.py
+++ Prog52A/Prog52A/MainForm.py
@@ -15,6 +15,9 @@ class MainForm(Form):
         self._label2 = System.Windows.Forms.Label()
         self._label3 = System.Windows.Forms.Label()
         self._label4 = System.Windows.Forms.Label()
+        self._button1 = System.Windows.Forms.Button()
+        self._button2 = System.Windows.Forms.Button()
+        self._button3 = System.Windows.Forms.Button()
         self.SuspendLayout()
         # 
         # label1
@@ -25,14 +28,14 @@ class MainForm(Form):
         self._label1.Name = "label1"
         self._label1.Size = System.Drawing.Size(293, 40)
         self._label1.TabIndex = 0
-        self._label1.Text = "Enter Area:"
+        self._label1.Text = "Enter Length:"
         # 
         # textBox1
         # 
         self._textBox1.Font = System.Drawing.Font("Microsoft Sans Serif", 21.75, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
-        self._textBox1.Location = System.Drawing.Point(194, 26)
+        self._textBox1.Location = System.Drawing.Point(209, 26)
         self._textBox1.Name = "textBox1"
-        self._textBox1.Size = System.Drawing.Size(677, 40)
+        self._textBox1.Size = System.Drawing.Size(662, 40)
         self._textBox1.TabIndex = 1
         # 
         # textBox2
@@ -73,10 +76,49 @@ class MainForm(Form):
         self._label4.TabIndex = 5
         self._label4.Text = "Perimeter:"
         # 
+        # button1
+        # 
+        self._button1.BackColor = System.Drawing.Color.FromArgb(255, 128, 128)
+        self._button1.Font = System.Drawing.Font("Microsoft Sans Serif", 27.75, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+        self._button1.Location = System.Drawing.Point(42, 376)
+        self._button1.Name = "button1"
+        self._button1.Size = System.Drawing.Size(184, 118)
+        self._button1.TabIndex = 6
+        self._button1.Text = "Calculate"
+        self._button1.UseVisualStyleBackColor = False
+        self._button1.Click += self.Button1Click
+        # 
+        # button2
+        # 
+        self._button2.BackColor = System.Drawing.Color.FromArgb(255, 128, 128)
+        self._button2.Font = System.Drawing.Font("Microsoft Sans Serif", 27.75, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+        self._button2.Location = System.Drawing.Point(348, 376)
+        self._button2.Name = "button2"
+        self._button2.Size = System.Drawing.Size(178, 118)
+        self._button2.TabIndex = 7
+        self._button2.Text = "Clear"
+        self._button2.UseVisualStyleBackColor = False
+        self._button2.Click += self.Button2Click
+        # 
+        # button3
+        # 
+        self._button3.BackColor = System.Drawing.Color.FromArgb(255, 128, 128)
+        self._button3.Font = System.Drawing.Font("Microsoft Sans Serif", 27.75, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+        self._button3.Location = System.Drawing.Point(693, 376)
+        self._button3.Name = "button3"
+        self._button3.Size = System.Drawing.Size(178, 118)
+        self._button3.TabIndex = 8
+        self._button3.Text = "Exit"
+        self._button3.UseVisualStyleBackColor = False
+        self._button3.Click += self.Button3Click
+        # 
         # MainForm
         # 
         self.BackColor = System.Drawing.Color.FromArgb(128, 128, 255)
         self.ClientSize = System.Drawing.Size(931, 587)
+        self.Controls.Add(self._button3)
+        self.Controls.Add(self._button2)
+        self.Controls.Add(self._button1)
         self.Controls.Add(self._label4)
         self.Controls.Add(self._label3)
         self.Controls.Add(self._textBox2)
@@ -88,3 +130,18 @@ class MainForm(Form):
         self.ResumeLayout(False)
         self.PerformLayout()
 
+
+    def Button3Click(self, sender, e):
+        Application.Exit()
+
+    def Button2Click(self, sender, e):
+        self._label3.Text = "Area: "
+        self._label4.Text = "Perimeter: "
+
+    def Button1Click(self, sender, e):
+        Len = float(self._textBox1.Text)
+        Wid = float(self._textBox2.Text)
+        Perim = 2 * Len + 2 * Wid
+        Area = Len * Wid
+        self._label3.Text = "Area: " + str(Area)
+        self._label4.Text = "Perimeter: " + str(Perim)
