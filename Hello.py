b5e2125af9bf20cd45248038e78bbfd8ba5e4098
carterc2209 <campbell.c@stu.janesville.k12.wi.us>
2025-02-04 20:45:02 +0000

    all the lp files

--- Hello.py
+++ Hello.py
@@ -1,1 +1,9 @@
 print("Hello, world!")
+# this is a single line comment
+""" Data Types:
+int - Integer (whole number)
+float - Floating-point number (number w/ a decimal)
+str - String(any text, in quotes)
+chr - Character (signle keyboard character, in single quotes)
+bool - Boolean (either True or False, only)
+"""
