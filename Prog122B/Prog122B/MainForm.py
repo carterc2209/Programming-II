e45a22af90e4342db42287504bed278738badf7e
carterc2209 <campbell.c@stu.janesville.k12.wi.us>
2025-02-07 14:40:40 -0600

    langdocs 2/7

---
+++ Prog122B/Prog122B/MainForm.py
@@ -0,0 +1,136 @@
+﻿import System.Drawing
+import System.Windows.Forms
+
+from System.Drawing import *
+from System.Windows.Forms import *
+
+class MainForm(Form):
+    def __init__(self):
+        self.InitializeComponent()
+    
+    def InitializeComponent(self):
+        self._button1 = System.Windows.Forms.Button()
+        self._button2 = System.Windows.Forms.Button()
+        self._button3 = System.Windows.Forms.Button()
+        self._label1 = System.Windows.Forms.Label()
+        self._label2 = System.Windows.Forms.Label()
+        self._textBox1 = System.Windows.Forms.TextBox()
+        self._textBox2 = System.Windows.Forms.TextBox()
+        self._listBox1 = System.Windows.Forms.ListBox()
+        self.SuspendLayout()
+        # 
+        # button1
+        # 
+        self._button1.BackColor = System.Drawing.Color.FromArgb(255, 128, 128)
+        self._button1.Font = System.Drawing.Font("Microsoft Sans Serif", 15.75, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+        self._button1.Location = System.Drawing.Point(21, 494)
+        self._button1.Name = "button1"
+        self._button1.Size = System.Drawing.Size(216, 91)
+        self._button1.TabIndex = 0
+        self._button1.Text = "Calculate"
+        self._button1.UseVisualStyleBackColor = False
+        self._button1.Click += self.Button1Click
+        # 
+        # button2
+        # 
+        self._button2.BackColor = System.Drawing.Color.FromArgb(255, 128, 128)
+        self._button2.Font = System.Drawing.Font("Microsoft Sans Serif", 15.75, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+        self._button2.Location = System.Drawing.Point(296, 494)
+        self._button2.Name = "button2"
+        self._button2.Size = System.Drawing.Size(216, 91)
+        self._button2.TabIndex = 1
+        self._button2.Text = "Clear"
+        self._button2.UseVisualStyleBackColor = False
+        self._button2.Click += self.Button2Click
+        # 
+        # button3
+        # 
+        self._button3.BackColor = System.Drawing.Color.FromArgb(255, 128, 128)
+        self._button3.Font = System.Drawing.Font("Microsoft Sans Serif", 15.75, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+        self._button3.Location = System.Drawing.Point(586, 494)
+        self._button3.Name = "button3"
+        self._button3.Size = System.Drawing.Size(216, 91)
+        self._button3.TabIndex = 2
+        self._button3.Text = "Exit"
+        self._button3.UseVisualStyleBackColor = False
+        self._button3.Click += self.Button3Click
+        # 
+        # label1
+        # 
+        self._label1.BackColor = System.Drawing.Color.White
+        self._label1.Font = System.Drawing.Font("Microsoft Sans Serif", 18, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+        self._label1.Location = System.Drawing.Point(21, 36)
+        self._label1.Name = "label1"
+        self._label1.Size = System.Drawing.Size(177, 40)
+        self._label1.TabIndex = 3
+        self._label1.Text = "Hourly Pay:"
+        # 
+        # label2
+        # 
+        self._label2.BackColor = System.Drawing.Color.White
+        self._label2.Font = System.Drawing.Font("Microsoft Sans Serif", 18, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+        self._label2.Location = System.Drawing.Point(21, 87)
+        self._label2.Name = "label2"
+        self._label2.Size = System.Drawing.Size(177, 40)
+        self._label2.TabIndex = 4
+        self._label2.Text = "Hours Worked:"
+        # 
+        # textBox1
+        # 
+        self._textBox1.Font = System.Drawing.Font("Microsoft Sans Serif", 21.75, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+        self._textBox1.Location = System.Drawing.Point(161, 36)
+        self._textBox1.Name = "textBox1"
+        self._textBox1.Size = System.Drawing.Size(641, 40)
+        self._textBox1.TabIndex = 5
+        # 
+        # textBox2
+        # 
+        self._textBox2.Font = System.Drawing.Font("Microsoft Sans Serif", 21.75, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+        self._textBox2.Location = System.Drawing.Point(192, 87)
+        self._textBox2.Name = "textBox2"
+        self._textBox2.Size = System.Drawing.Size(610, 40)
+        self._textBox2.TabIndex = 6
+        # 
+        # listBox1
+        # 
+        self._listBox1.Font = System.Drawing.Font("Microsoft Sans Serif", 14.25, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+        self._listBox1.FormattingEnabled = True
+        self._listBox1.ItemHeight = 24
+        self._listBox1.Location = System.Drawing.Point(20, 144)
+        self._listBox1.Name = "listBox1"
+        self._listBox1.Size = System.Drawing.Size(781, 292)
+        self._listBox1.TabIndex = 7
+        # 
+        # MainForm
+        # 
+        self.BackColor = System.Drawing.Color.FromArgb(128, 128, 255)
+        self.ClientSize = System.Drawing.Size(831, 597)
+        self.Controls.Add(self._listBox1)
+        self.Controls.Add(self._textBox2)
+        self.Controls.Add(self._textBox1)
+        self.Controls.Add(self._label2)
+        self.Controls.Add(self._label1)
+        self.Controls.Add(self._button3)
+        self.Controls.Add(self._button2)
+        self.Controls.Add(self._button1)
+        self.Name = "MainForm"
+        self.Text = "Prog122B"
+        self.ResumeLayout(False)
+        self.PerformLayout()
+
+
+    def Button3Click(self, sender, e):
+        Application.Exit()
+
+    def Button2Click(self, sender, e):
+        self._listBox1.Items.Clear()
+
+    def Button1Click(self, sender, e):
+        heading = "Pay\t\tHours"
+        self._listBox1.Items.Add(heading)
+        Hours = int(self._textBox2.Text)
+        Pay = int(self._textBox1.Text)
+        TTL = int(Hours * Pay)
+        for i in range(1, Hours+1):
+            line = str(Hours) + "\t\t" + str(TTL)
+            self._listBox1.Items.Add(line)
