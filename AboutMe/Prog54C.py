da19f1e1b85226fe2c9ee91be2135f60e2216856
carterc2209 <campbell.c@stu.janesville.k12.wi.us>
2025-02-17 14:46:37 -0600

    prog54c

--- AboutMe/Prog54C.py
+++ AboutMe/Prog54C.py
@@ -1,14 +1,20 @@
-radius = int(input("Enter the Radius: "))
+radius = float(input("Enter the Radius: "))
 pi = 3.14159
 
 
 def circumference():
-    cir = 2 * pi * radius
-    print(cir)
+    cir = (2 * pi * radius)
+    print("Circumference: ", cir)
+
+
+def area():
+    are = (pi * radius**2)
+    print("Area: ", are)
 
 
 def main():
     circumference()
+    area()
 
 
 if __name__ == "__main__":
