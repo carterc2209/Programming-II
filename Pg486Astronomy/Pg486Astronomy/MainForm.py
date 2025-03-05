80298690e1fd88701790bb047802b57bd31c63d2
carterc2209 <campbell.c@stu.janesville.k12.wi.us>
2025-03-05 14:55:54 -0600

    Pg486Astronomy 3/5

--- Pg486Astronomy/Pg486Astronomy/MainForm.py
+++ Pg486Astronomy/Pg486Astronomy/MainForm.py
@@ -14,7 +14,6 @@ class MainForm(Form):
         self._label2 = System.Windows.Forms.Label()
         self._button1 = System.Windows.Forms.Button()
         self._button2 = System.Windows.Forms.Button()
-        self._button3 = System.Windows.Forms.Button()
         self.SuspendLayout()
         # 
         # label1
@@ -69,42 +68,30 @@ Surface Temp
         # 
         self._button1.BackColor = System.Drawing.Color.FromArgb(255, 128, 128)
         self._button1.Font = System.Drawing.Font("Microsoft Sans Serif", 27.75, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
-        self._button1.Location = System.Drawing.Point(165, 382)
+        self._button1.Location = System.Drawing.Point(14, 382)
         self._button1.Name = "button1"
-        self._button1.Size = System.Drawing.Size(118, 128)
+        self._button1.Size = System.Drawing.Size(195, 128)
         self._button1.TabIndex = 3
         self._button1.Text = "Clear"
         self._button1.UseVisualStyleBackColor = False
+        self._button1.Click += self.Button1Click
         # 
         # button2
         # 
         self._button2.BackColor = System.Drawing.Color.FromArgb(255, 128, 128)
         self._button2.Font = System.Drawing.Font("Microsoft Sans Serif", 27.75, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
-        self._button2.Location = System.Drawing.Point(318, 382)
+        self._button2.Location = System.Drawing.Point(232, 382)
         self._button2.Name = "button2"
-        self._button2.Size = System.Drawing.Size(114, 128)
+        self._button2.Size = System.Drawing.Size(200, 128)
         self._button2.TabIndex = 4
         self._button2.Text = "Exit"
         self._button2.UseVisualStyleBackColor = False
         self._button2.Click += self.Button2Click
         # 
-        # button3
-        # 
-        self._button3.BackColor = System.Drawing.Color.FromArgb(255, 128, 128)
-        self._button3.Font = System.Drawing.Font("Microsoft Sans Serif", 21.75, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
-        self._button3.Location = System.Drawing.Point(14, 382)
-        self._button3.Name = "button3"
-        self._button3.Size = System.Drawing.Size(128, 128)
-        self._button3.TabIndex = 5
-        self._button3.Text = "Change"
-        self._button3.UseVisualStyleBackColor = False
-        self._button3.Click += self.Button3Click
-        # 
         # MainForm
         # 
         self.BackColor = System.Drawing.Color.FromArgb(128, 128, 255)
         self.ClientSize = System.Drawing.Size(444, 522)
-        self.Controls.Add(self._button3)
         self.Controls.Add(self._button2)
         self.Controls.Add(self._button1)
         self.Controls.Add(self._label2)
@@ -123,22 +110,24 @@ Surface Temp
         if combo == "Mercury":
             self._label2.Text = "Type: Terrestrial" + "\n\n" + "Avg Distance From the Sun: 0.387 AU" + "\n\n" + "Mass: 3.31 x 10^23 kg" + "\n\n" + "Surface Tempurature: -173°C to 420°C"
         elif combo == "Venus":
-            self._label2.Text = "Type: Terrestrial" + "\n\n" + "Avg Distance From the Sun: 0.7233 AU" + "\n\n" + "Mass: 3.31 x 10^23 kg" + "\n\n" + "Surface Tempurature: -173°C to 420°C"
+            self._label2.Text = "Type: Terrestrial" + "\n\n" + "Avg Distance From the Sun: 0.7233 AU" + "\n\n" + "Mass: 4.87 x 10^24 kg" + "\n\n" + "Surface Tempurature: 472°C"
         elif combo == "Earth":
-            self._label2.Text = "Type: Terrestrial" + "\n\n" + "Avg Distance From the Sun: 1 AU" + "\n\n" + "Mass: 3.31 x 10^23 kg" + "\n\n" + "Surface Tempurature: -173°C to 420°C"
+            self._label2.Text = "Type: Terrestrial" + "\n\n" + "Avg Distance From the Sun: 1 AU" + "\n\n" + "Mass: 5.967 x 10^24 kg" + "\n\n" + "Surface Tempurature: –50°C to 50°C"
         elif combo == "Mars":
-            self._label2.Text = "Type: Terrestrial" + "\n\n" + "Avg Distance From the Sun: 1.5237 AU" + "\n\n" + "Mass: 3.31 x 10^23 kg" + "\n\n" + "Surface Tempurature: -173°C to 420°C"
+            self._label2.Text = "Type: Terrestrial" + "\n\n" + "Avg Distance From the Sun: 1.5237 AU" + "\n\n" + "Mass: 0.6424 x 10^24 kg" + "\n\n" + "Surface Tempurature: –140°C to 20°C"
         elif combo == "Jupiter":
-            self._label2.Text = "Type: Jovian" + "\n\n" + "Avg Distance From the Sun: 5.2028 AU" + "\n\n" + "Mass: 3.31 x 10^23 kg" + "\n\n" + "Surface Tempurature: -173°C to 420°C"
+            self._label2.Text = "Type: Jovian" + "\n\n" + "Avg Distance From the Sun: 5.2028 AU" + "\n\n" + "Mass: 1.899 x 10^27 kg" + "\n\n" + "Surface Tempurature: –110°C"
         elif combo == "Saturn":
-            self._label2.Text = "Type: Jovian" + "\n\n" + "Avg Distance From the Sun: 9.5388 AU" + "\n\n" + "Mass: 3.31 x 10^23 kg" + "\n\n" + "Surface Tempurature: -173°C to 420°C"
+            self._label2.Text = "Type: Jovian" + "\n\n" + "Avg Distance From the Sun: 9.5388 AU" + "\n\n" + "Mass: 5.69 x 10^26 kg" + "\n\n" + "Surface Tempurature: –180°C"
         elif combo == "Uranus":
-            self._label2.Text = "Type: Jovian" + "\n\n" + "Avg Distance From the Sun: 19.18 AU" + "\n\n" + "Mass: 3.31 x 10^23 kg" + "\n\n" + "Surface Tempurature: -173°C to 420°C"
+            self._label2.Text = "Type: Jovian" + "\n\n" + "Avg Distance From the Sun: 19.18 AU" + "\n\n" + "Mass: 8.69 x 10^25 kg" + "\n\n" + "Surface Tempurature: –220°C"
         elif combo == "Neptune":
-            self._label2.Text = "Type: Jovian" + "\n\n" + "Avg Distance From the Sun: 30.0611 AU" + "\n\n" + "Mass: 3.31 x 10^23 kg" + "\n\n" + "Surface Tempurature: -173°C to 420°C"
+            self._label2.Text = "Type: Jovian" + "\n\n" + "Avg Distance From the Sun: 30.0611 AU" + "\n\n" + "Mass: 1.03 x 10^26 kg" + "\n\n" + "Surface Tempurature: –216°C"
         elif combo == "Pluto":
-            self._label2.Text = "Type: Low Density" + "\n\n" + "Avg Distance From the Sun: 0.387 AU" + "\n\n" + "Mass: 3.31 x 10^23 kg" + "\n\n" + "Surface Tempurature: -173°C to 420°C"
+            self._label2.Text = "Type: Low Density" + "\n\n" + "Avg Distance From the Sun: 0.387 AU" + "\n\n" + "Mass: 1.2 x 10^22 kg" + "\n\n" + "Surface Tempurature: –230°C"
             
 
-    def Button3Click(self, sender, e):
-        pass
+    def Button1Click(self, sender, e):
+        self._comboBox1.Text = ""
+        self._label2.Text = "Type: " + "\n\n" + "Avg Distance From the Sun: " + "\n\n" + "Mass: " + "\n\n" + "Surface Tempurature: "
+        
