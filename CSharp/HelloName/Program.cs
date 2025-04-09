b24e75f5e74c7e3c9653123408e661445be7ed21
carterc2209 <campbell.c@stu.janesville.k12.wi.us>
2025-04-09 15:09:54 -0500

    CSharp Intro

---
+++ CSharp/HelloName/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+Console.Write("Enter your name: ");
+// int, bool, string, char, double instead of float
+string name = Console.ReadLine();
+Console.WriteLine("Hello," + name);
+
+Console.WriteLine("Enter your age: ");
+// int age = convert.ToInt32(Console.ReadLine());
+int age = int.Parse(Console.ReadLine());
+// age = int(input)
+Console.WriteLine("You are: " + age + "years old");
