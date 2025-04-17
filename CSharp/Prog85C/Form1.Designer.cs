76b17eec6f6a65a357b7febfdcd398d28ace4850
carterc2209 <campbell.c@stu.janesville.k12.wi.us>
2025-04-17 15:26:16 -0500

    csharp stuff

--- CSharp/Prog85C/Form1.Designer.cs
+++ CSharp/Prog85C/Form1.Designer.cs
@@ -33,6 +33,8 @@
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -74,12 +76,34 @@
             // 
             // label3
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(63, 211);
+            this.label3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(12, 185);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(38, 15);
+            this.label3.Size = new System.Drawing.Size(776, 122);
             this.label3.TabIndex = 4;
-            this.label3.Text = "label3";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(61, 346);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(188, 74);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Calculate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Segoe UI", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button2.Location = new System.Drawing.Point(524, 346);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(188, 74);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Clear";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // Form1
             // 
@@ -87,6 +111,8 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
@@ -106,5 +132,7 @@
         private TextBox textBox1;
         private TextBox textBox2;
         private Label label3;
+        private Button button1;
+        private Button button2;
     }
 }
--- CSharp/Prog85C/Form1.cs
+++ CSharp/Prog85C/Form1.cs
@@ -11,5 +11,71 @@ namespace Prog85C
         {
 
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+            label3.Text = "";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int month = 0;
+            string opt1 = textBox2.Text;
+            if (opt1 == "January" || opt1 == "1")
+            {
+                month = 1;
+            }
+            else if (opt1 == "February" || opt1 == "2")
+            {
+                month = 2;
+            }
+            else if (opt1 == "March" || opt1 == "3")
+            {
+                month = 3;
+            }
+            else if (opt1 == "April" || opt1 == "4")
+            {
+                month = 4;
+            }
+            else if (opt1 == "May" || opt1 == "5")
+            {
+                month = 5;
+            }
+            else if (opt1 == "June" || opt1 == "6")
+            {
+                month = 6;
+            }
+            else if (opt1 == "July" || opt1 == "7")
+            {
+                month = 7;
+            }
+            else if (opt1 == "August" || opt1 == "8")
+            {
+                month = 8;
+            }
+            else if (opt1 == "September" || opt1 == "9")
+            {
+                month = 9;
+            }
+            else if (opt1 == "October" || opt1 == "10")
+            {
+                month = 10;
+            }
+            else if (opt1 == "November" || opt1 == "11")
+            {
+                month = 11;
+            }
+            else if (opt1 == "December" || opt1 == "12")
+            {
+                month = 12;
+            }
+            int day = int.Parse(textBox1.Text);
+            int num = (((((month * 5) + 6) * 4) + 9) * 5) + day;
+            int birthdate = (num - 165) / 100;
+            label3.Text = num.ToString() + "\n" + birthdate.ToString() + "/" + day;
+ 
+        }
     }
 }
