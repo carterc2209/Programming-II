22be50f1136f93ffbecd438fa52d6985f6eea5e4
carterc2209 <campbell.c@stu.janesville.k12.wi.us>
2025-05-14 15:27:42 -0500

    asdsadsa

--- CSharp/StrInt18/Program.cs
+++ CSharp/StrInt18/Program.cs
@@ -1,2 +1,7 @@
-﻿Console.Write("Enter a string");
-Console.Read();
+﻿Console.Write("Enter a string: ");
+string line1 = Console.ReadLine();
+List<string> beans = new List<string>();
+Console.WriteLine("Enter a character to remove: ");
+string cha = Console.ReadLine();
+beans.Remove(cha);
+Console.WriteLine(beans);
